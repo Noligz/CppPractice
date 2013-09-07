c5ffb9b8e8b5e380a28f25836c74c4a18d562cb2
Noligz <noligz@live.cn>
2013-09-07 15:17:16 +0800

    timing & BitABS

--- CppPractice/FuncTest/FuncTest.cpp
+++ CppPractice/FuncTest/FuncTest.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 void f1()
@@ -19,9 +20,37 @@ void f2()
 	cout << strlen(c) << endl;
 }
 
+int BitABS(int n)
+{
+	int shiftRight = sizeof(n) * 8 - 1;
+	int signBit = n >> shiftRight;
+	int ret = n^signBit + signBit;
+	return ret;
+}
+
+int IfABS(int n)
+{
+	if (n >= 0)
+		return n;
+	else
+		return -n;
+}
+
 void main()
 {
-	f2();
+	clock_t startClock, finishClock;
+	startClock = clock();
+
+	for (size_t i = 0; i < 100000000; i++)
+	{
+		//int b = abs(-345);
+		//int a=BitABS(-345);
+		int c = IfABS(-345);
+	}
+	
+
+	finishClock = clock();
+	cout << endl << "Duration: " << (double) (finishClock - startClock) / 1000 << endl;
 
 	system("pause");
 }
